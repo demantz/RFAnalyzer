2337d77215885656d89ffa34484d781f95bba88e
Dennis Mantz <dennis.mantz@googlemail.com>
2014-10-19 00:07:52 -0700

    Added file chooser for filesource setting

--- src/main/java/com/mantz_it/rfanalyzer/SettingsFragment.java
+++ src/main/java/com/mantz_it/rfanalyzer/SettingsFragment.java
@@ -1,12 +1,22 @@
 package com.mantz_it.rfanalyzer;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * <h1>RF Analyzer - Settings Fragment</h1>
@@ -33,12 +43,18 @@ import android.preference.SwitchPreference;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener,
+																	Preference.OnPreferenceClickListener {
+
+	private static final int FILESOURCE_RESULT_CODE = 1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+
+		EditTextPreference filesource = (EditTextPreference) findPreference(getString(R.string.pref_filesource_file));
+		filesource.setOnPreferenceClickListener(this);
 	}
 
 	@Override
@@ -56,6 +72,49 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 	public void onPause() {
 		super.onPause();
 		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		if(preference.getKey().equals(getString(R.string.pref_filesource_file))) {
+			try {
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.setType("*/*");
+				intent.addCategory(Intent.CATEGORY_OPENABLE);
+				startActivityForResult(Intent.createChooser(intent, "Select a file (8-bit complex IQ samples)"), FILESOURCE_RESULT_CODE);
+
+				// No error so far... let's dismiss the text input dialog:
+				Dialog dialog = ((EditTextPreference)preference).getDialog();
+				if(dialog != null)
+					dialog.dismiss();
+				return true;
+			} catch (ActivityNotFoundException e) {
+				Toast.makeText(SettingsFragment.this.getActivity(), "No file browser is installed!", Toast.LENGTH_LONG).show();
+				// Note that there is still the text dialog visible for the user to input a file path... so no more error handling necessary
+			}
+			return false;
+		}
+		return false;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(data != null) {
+			switch (requestCode) {
+				case FILESOURCE_RESULT_CODE:
+					Uri uri = data.getData();
+					if (uri != null) {
+						if(uri.toString().startsWith("file://"))
+							((EditTextPreference) findPreference(getString(R.string.pref_filesource_file)))
+										.setText(uri.toString().substring(7));
+						else
+							Toast.makeText(SettingsFragment.this.getActivity(), "Not a local file: " + uri.toString(), Toast.LENGTH_LONG).show();
+					}
+					break;
+				default:
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
 	@Override
@@ -120,5 +179,21 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 			getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
 		else if(orientation.equals("reverse_portrait"))
 			getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+	}
+
+	/**
+	 * Extract the path from an uri
+	 * This code was published on StackOverflow by dextor
+	 *
+	 * @param contentUri		uri that contains the file path
+	 * @return absolute file path as string
+	 */
+	private String getRealPathFromURI(Uri contentUri) {
+		String[] proj = { MediaStore.Images.Media.DATA };
+		CursorLoader loader = new CursorLoader(this.getActivity(), contentUri, proj, null, null, null);
+		Cursor cursor = loader.loadInBackground();
+		int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		cursor.moveToFirst();
+		return cursor.getString(column_index);
 	}
 }
