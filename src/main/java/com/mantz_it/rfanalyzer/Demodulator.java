7c1ef669cbe5e9586661e1efa5953bc1b8e5ab65
Dennis Mantz <dennis.mantz@googlemail.com>
2014-10-28 10:09:44 -0700

    Put decimation process in seperate thread

--- src/main/java/com/mantz_it/rfanalyzer/Demodulator.java
+++ src/main/java/com/mantz_it/rfanalyzer/Demodulator.java
@@ -43,17 +43,8 @@ public class Demodulator extends Thread {
 													8*AUDIO_RATE};	// wFM
 	public static final int INPUT_RATE = 1000000;
 
-	private ArrayBlockingQueue<SamplePacket> inputQueue;
-	private ArrayBlockingQueue<SamplePacket> outputQueue;
-	private int packetSize;
-
-	// DOWNSAMPLING:
-	private HalfBandLowPassFilter inputFilter1 = null;
-	private HalfBandLowPassFilter inputFilter2 = null;
-	private HalfBandLowPassFilter inputFilter3 = null;
-	private FirFilter inputFilter4 = null;
-	private SamplePacket downsampledSamples;
-	private SamplePacket tmpDownsampledSamples;
+	// DECIMATION
+	private Decimator decimator;
 
 	// FILTERING
 	private static final int USER_FILTER_ATTENUATION = 20;
@@ -70,7 +61,6 @@ public class Demodulator extends Thread {
 														120000};// wFM
 
 	// DEMODULATION
-	private SamplePacket demodulatedSamples;
 	private SamplePacket demodulatorHistory;
 	public static final int DEMODULATION_OFF = 0;
 	public static final int DEMODULATION_AM = 1;
@@ -82,26 +72,19 @@ public class Demodulator extends Thread {
 	private AudioSink audioSink = null;
 
 	public Demodulator (ArrayBlockingQueue<SamplePacket> inputQueue, ArrayBlockingQueue<SamplePacket> outputQueue, int packetSize) {
-		this.inputQueue = inputQueue;
-		this.outputQueue = outputQueue;
-		this.packetSize = packetSize;
-
 		// Create internal sample buffers:
 		// Note that we create the buffers for the case that there is no downsampling necessary
 		// All other cases with input decimation > 1 are also possible because they only need
 		// smaller buffers.
-		this.downsampledSamples = new SamplePacket(packetSize);
-		this.tmpDownsampledSamples = new SamplePacket(packetSize);
 		this.quadratureSamples = new SamplePacket(packetSize);
-		this.demodulatedSamples = new SamplePacket(packetSize);
 
 		// Create Audio Sink
 		this.audioSink = new AudioSink(packetSize, AUDIO_RATE);
 
-		// Create half band filters for downsampling:
-		this.inputFilter1 = new HalfBandLowPassFilter(8);
-		this.inputFilter2 = new HalfBandLowPassFilter(8);
-		this.inputFilter3 = new HalfBandLowPassFilter(8);
+		// Create Decimator block
+		// Note that the decimator directly reads from the inputQueue and also returns processed packets to the
+		// output queue.
+		this.decimator = new Decimator(QUADRATURE_RATE[demodulationMode], packetSize, inputQueue, outputQueue);
 	}
 
 	public int getDemodulationMode() {
@@ -113,6 +96,7 @@ public class Demodulator extends Thread {
 			Log.e(LOGTAG,"setDemodulationMode: invalid mode: " + demodulationMode);
 			return;
 		}
+		this.decimator.setOutputSampleRate(QUADRATURE_RATE[demodulationMode]);
 		this.demodulationMode = demodulationMode;
 		this.userFilterCutOff = MAX_USER_FILTER_WIDTH[demodulationMode];
 	}
@@ -161,34 +145,20 @@ public class Demodulator extends Thread {
 
 		while (!stopRequested) {
 
-			try {
-				inputSamples = inputQueue.poll(1000, TimeUnit.MILLISECONDS);
-			} catch (InterruptedException e) {
-				Log.e(LOGTAG,"run: Interrupted while waiting on input queue! stop.");
-				this.stopRequested = true;
-				break;
-			}
+			// Get downsampled packet from the decimator:
+			inputSamples = decimator.getDecimatedPacket(1000);
 
 			// Verify the input sample packet is not null:
 			if (inputSamples == null) {
-				Log.d(LOGTAG, "run: Input sample is null. skip this round...");
+				Log.d(LOGTAG, "run: Decimated sample is null. skip this round...");
 				continue;
 			}
-
-			// Verify the input sample rate:
-			if (inputSamples.getSampleRate() != INPUT_RATE) {
-				Log.d(LOGTAG, "run: Input sample rate is " + inputSamples.getSampleRate() + " but should be" + INPUT_RATE + ". skip.");
-				continue;
-			}
-
-			// downsampling		[sample rate decimated from INPUT_RATE to QUADRATURE_RATE]
-			downsampling(inputSamples, downsampledSamples);				// The result from downsampling is stored in downsampledSamples
-
-			// return inputSamples back to the Scheduler:
-			outputQueue.offer(inputSamples);
 
 			// filtering		[sample rate is QUADRATURE_RATE]
-			applyUserFilter(downsampledSamples, quadratureSamples);		// The result from filtering is stored in quadratureSamples
+			applyUserFilter(inputSamples, quadratureSamples);		// The result from filtering is stored in quadratureSamples
+
+			// return input samples to the decimator block:
+			decimator.returnDecimatedPacket(inputSamples);
 
 			// get buffer from audio sink
 			audioBuffer = audioSink.getPacketBuffer(1000);
@@ -205,42 +175,6 @@ public class Demodulator extends Thread {
 
 		this.stopRequested = true;
 		Log.i(LOGTAG,"Demodulator stopped. (Thread: " + this.getName() + ")");
-	}
-
-	public void downsampling(SamplePacket input, SamplePacket output) {
-		// Verify that the input filter 4 is still correct configured (gain):
-		if(inputFilter4 == null || inputFilter4.getGain() != 2*(QUADRATURE_RATE[demodulationMode]/(double)INPUT_RATE) ) {
-			// We have to (re-)create the filter:
-			this.inputFilter4 = FirFilter.createLowPass(2, 2*(QUADRATURE_RATE[demodulationMode]/(double)INPUT_RATE), 1, 0.15, 0.2, 20);
-			Log.d(LOGTAG,"downsampling: created new inputFilter4 with " + inputFilter4.getNumberOfTaps()
-					+ " taps. Decimation=" + inputFilter4.getDecimation() + " Cut-Off="+inputFilter4.getCutOffFrequency()
-					+ " transition="+inputFilter4.getTransitionWidth());
-		}
-
-		// apply first filter (decimate to INPUT_RATE/2)
-		tmpDownsampledSamples.setSize(0);	// mark buffer as empty
-		if (inputFilter1.filterN8(input, tmpDownsampledSamples, 0, input.size()) < input.size()) {
-			Log.e(LOGTAG, "downsampling: [inputFilter1] could not filter all samples from input packet.");
-		}
-
-		// if we need a decimation of 16: apply second and third filter (decimate to INPUT_RATE/8)
-		if(INPUT_RATE/QUADRATURE_RATE[demodulationMode] == 16) {
-			output.setSize(0);	// mark buffer as empty
-			if (inputFilter2.filterN8(tmpDownsampledSamples, output, 0, tmpDownsampledSamples.size()) < tmpDownsampledSamples.size()) {
-				Log.e(LOGTAG, "downsampling: [inputFilter2] could not filter all samples from input packet.");
-			}
-
-			tmpDownsampledSamples.setSize(0);	// mark tmp buffer as again
-			if (inputFilter3.filterN8(output, tmpDownsampledSamples, 0, output.size()) < output.size()) {
-				Log.e(LOGTAG, "downsampling: [inputFilter3] could not filter all samples from input packet.");
-			}
-		}
-
-		// apply fourth filter (decimate either to INPUT_RATE/4 or INPUT_RATE/16)
-		output.setSize(0);	// mark buffer as empty
-		if (inputFilter4.filter(tmpDownsampledSamples, output, 0, tmpDownsampledSamples.size()) < tmpDownsampledSamples.size()) {
-			Log.e(LOGTAG, "downsampling: [inputFilter4] could not filter all samples from input packet.");
-		}
 	}
 
 	public void applyUserFilter(SamplePacket input, SamplePacket output) {
