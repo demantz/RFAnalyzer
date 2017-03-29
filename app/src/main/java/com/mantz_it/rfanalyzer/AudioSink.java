2ab678b5ef46c47fef46324f848a78634414bad7
Pavel Remygailo <pavel.remygailo+github@gmail.com>
2017-03-29 13:19:29 +0300

    Now implements Runnable instead of extending Thread

--- app/src/main/java/com/mantz_it/rfanalyzer/AudioSink.java
+++ app/src/main/java/com/mantz_it/rfanalyzer/AudioSink.java
@@ -37,197 +37,199 @@ import java.util.concurrent.TimeUnit;
  *         License along with this library; if not, write to the Free Software
  *         Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-public class AudioSink extends Thread {
-	private AudioTrack audioTrack = null;        // AudioTrack object that is used to pass audio samples to the Android system
-	private boolean stopRequested = true;
-	private ArrayBlockingQueue<SamplePacket> inputQueue = null;        // Queue that holds incoming samples
-	private ArrayBlockingQueue<SamplePacket> outputQueue = null;    // Queue that holds available buffers
-	private int packetSize;        // packet size of the incoming sample packets
-	private int sampleRate;        // audio sample rate of the AudioSink
-	private static final int QUEUE_SIZE = 2;    // This results in a double buffer. see Scheduler...
-	private static final String LOGTAG = "AudioSink";
-	private FirFilter audioFilter1 = null;        // Filter used to decimate the incoming signal rate
-	private FirFilter audioFilter2 = null;        // Cascaded filter for high incoming signal rates
-	private SamplePacket tmpAudioSamples;        // tmp buffer for audio filters.
+public class AudioSink implements Runnable {
+private Thread thread;
+private AudioTrack audioTrack = null;        // AudioTrack object that is used to pass audio samples to the Android system
+private boolean stopRequested = true;
+private ArrayBlockingQueue<SamplePacket> inputQueue = null;        // Queue that holds incoming samples
+private ArrayBlockingQueue<SamplePacket> outputQueue = null;    // Queue that holds available buffers
+private int packetSize;        // packet size of the incoming sample packets
+private int sampleRate;        // audio sample rate of the AudioSink
+private static final int QUEUE_SIZE = 2;    // This results in a double buffer. see Scheduler...
+private static final String LOGTAG = "AudioSink";
+private FirFilter audioFilter1 = null;        // Filter used to decimate the incoming signal rate
+private FirFilter audioFilter2 = null;        // Cascaded filter for high incoming signal rates
+private SamplePacket tmpAudioSamples;        // tmp buffer for audio filters.
 
-	/**
-	 * Constructor. Will create a new AudioSink.
-	 *
-	 * @param packetSize size of the incoming packets
-	 * @param sampleRate sample rate of the audio signal
-	 */
-	public AudioSink(int packetSize, int sampleRate) {
-		super("AudioSink Thread");
-		Log.v(LOGTAG, "constructor: packetSize: " + packetSize + ", sampleRate: " + sampleRate);
-		this.packetSize = packetSize;
-		this.sampleRate = sampleRate;
+/**
+ * Constructor. Will create a new AudioSink.
+ *
+ * @param packetSize size of the incoming packets
+ * @param sampleRate sample rate of the audio signal
+ */
+public AudioSink(int packetSize, @SuppressWarnings("SameParameterValue") int sampleRate) {
+	thread = new Thread(this, "AudioSink Thread");
+	Log.v(LOGTAG, "constructor: packetSize: " + packetSize + ", sampleRate: " + sampleRate);
+	this.packetSize = packetSize;
+	this.sampleRate = sampleRate;
 
-		// Create the queues and fill them with
-		this.inputQueue = new ArrayBlockingQueue<SamplePacket>(QUEUE_SIZE);
-		this.outputQueue = new ArrayBlockingQueue<SamplePacket>(QUEUE_SIZE);
-		for (int i = 0; i < QUEUE_SIZE; i++)
-			this.outputQueue.offer(new SamplePacket(packetSize));
-		// Create an instance of the AudioTrack class:
-		int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-		this.audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-				AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+	// Create the queues and fill them with
+	this.inputQueue = new ArrayBlockingQueue<>(QUEUE_SIZE);
+	this.outputQueue = new ArrayBlockingQueue<>(QUEUE_SIZE);
+	for (int i = 0; i < QUEUE_SIZE; i++)
+		this.outputQueue.offer(new SamplePacket(packetSize));
+	// Create an instance of the AudioTrack class:
+	int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+	this.audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+			AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
 
-		// Create the audio filters:
-		this.audioFilter1 = FirFilter.createLowPass(2, 1, 1, 0.1f, 0.15f, 30);
-		Log.d(LOGTAG, "constructor: created audio filter 1 with " + audioFilter1.getNumberOfTaps() + " Taps.");
-		this.audioFilter2 = FirFilter.createLowPass(4, 1, 1, 0.1f, 0.1f, 30);
-		Log.d(LOGTAG, "constructor: created audio filter 2 with " + audioFilter2.getNumberOfTaps() + " Taps.");
-		this.tmpAudioSamples = new SamplePacket(packetSize);
+	// Create the audio filters:
+	this.audioFilter1 = FirFilter.createLowPass(2, 1, 1, 0.1f, 0.15f, 30);
+	Log.d(LOGTAG, "constructor: created audio filter 1 with " + audioFilter1.getNumberOfTaps() + " Taps.");
+	this.audioFilter2 = FirFilter.createLowPass(4, 1, 1, 0.1f, 0.1f, 30);
+	Log.d(LOGTAG, "constructor: created audio filter 2 with " + audioFilter2.getNumberOfTaps() + " Taps.");
+	this.tmpAudioSamples = new SamplePacket(packetSize);
+}
+
+/**
+ * Starts the thread
+ */
+public synchronized void start() {
+	stopRequested = false;
+	thread.start();
+}
+
+/**
+ * Stops the thread
+ */
+public void stopSink() {
+	stopRequested = true;
+}
+
+/**
+ * @return size of the packets that are offered by getPacketBuffer()
+ */
+public int getPacketSize() {
+	return packetSize;
+}
+
+/**
+ * The AudioSink allocates the buffers for audio playback. Use this method to request
+ * a free buffer. This method will block if no buffer is available.
+ *
+ * @param timeout max time this method will block
+ * @return free buffer or null if no buffer available
+ */
+public SamplePacket getPacketBuffer(int timeout) {
+	try {
+		return outputQueue.poll(timeout, TimeUnit.MILLISECONDS);
 	}
-
-	/**
-	 * Starts the thread
-	 */
-	@Override
-	public synchronized void start() {
-		stopRequested = false;
-		super.start();
+	catch (InterruptedException e) {
+		Log.e(LOGTAG, "getPacketBuffer: Interrupted. return null...");
+		return null;
 	}
+}
 
-	/**
-	 * Stops the thread
-	 */
-	public void stopSink() {
-		stopRequested = true;
+/**
+ * Enqueues a packet buffer for being played on the audio track.
+ *
+ * @param packet the packet buffer from getPacketBuffer() filled with samples
+ * @return true if success, false if error
+ */
+public boolean enqueuePacket(SamplePacket packet) {
+	if (packet == null) {
+		Log.e(LOGTAG, "enqueuePacket: InterleavedPacket is null.");
+		return false;
 	}
-
-	/**
-	 * @return size of the packets that are offered by getPacketBuffer()
-	 */
-	public int getPacketSize() {
-		return packetSize;
+	if (!inputQueue.offer(packet)) {
+		Log.e(LOGTAG, "enqueuePacket: Queue is full.");
+		return false;
 	}
+	return true;
+}
 
-	/**
-	 * The AudioSink allocates the buffers for audio playback. Use this method to request
-	 * a free buffer. This method will block if no buffer is available.
-	 *
-	 * @param timeout max time this method will block
-	 * @return free buffer or null if no buffer available
-	 */
-	public SamplePacket getPacketBuffer(int timeout) {
+short[] shortPacket;
+
+@Override
+public void run() {
+	SamplePacket packet;
+	SamplePacket filteredPacket;
+	SamplePacket tempPacket = new SamplePacket(packetSize);
+	float[] floatPacket;
+	shortPacket = new short[packetSize];
+
+	Log.i(LOGTAG, "AudioSink started. (Thread: " + thread.getName() + ")");
+
+	// start audio playback:
+	audioTrack.play();
+
+	// Continuously write the data from the queue to the audio track:
+	while (!stopRequested) {
 		try {
-			return outputQueue.poll(timeout, TimeUnit.MILLISECONDS);
-		} catch (InterruptedException e) {
-			Log.e(LOGTAG, "getPacketBuffer: Interrupted. return null...");
-			return null;
-		}
-	}
+			// Get the next packet from the queue
+			packet = inputQueue.poll(1000, TimeUnit.MILLISECONDS);
 
-	/**
-	 * Enqueues a packet buffer for being played on the audio track.
-	 *
-	 * @param packet the packet buffer from getPacketBuffer() filled with samples
-	 * @return true if success, false if error
-	 */
-	public boolean enqueuePacket(SamplePacket packet) {
-		if (packet == null) {
-			Log.e(LOGTAG, "enqueuePacket: InterleavedPacket is null.");
-			return false;
-		}
-		if (!inputQueue.offer(packet)) {
-			Log.e(LOGTAG, "enqueuePacket: Queue is full.");
-			return false;
-		}
-		return true;
-	}
+			if (packet == null) {
+				//Log.d(LOGTAG, "run: Queue is empty. skip this round");
+				continue;
+			}
+			//Log.v(LOGTAG, "packet rate: " + packet.getSampleRate()+'('+this.sampleRate+'*'+(packet.getSampleRate()/this.sampleRate)+')');
+			// apply audio filter (decimation)
+			if (packet.getSampleRate() > this.sampleRate) {
+				applyAudioFilter(packet, tempPacket);
+				filteredPacket = tempPacket;
+			} else
+				filteredPacket = packet;
 
-	short[] shortPacket;
-
-	@Override
-	public void run() {
-		SamplePacket packet;
-		SamplePacket filteredPacket;
-		SamplePacket tempPacket = new SamplePacket(packetSize);
-		float[] floatPacket;
-		shortPacket = new short[packetSize];
-
-		Log.i(LOGTAG, "AudioSink started. (Thread: " + this.getName() + ")");
-
-		// start audio playback:
-		audioTrack.play();
-
-		// Continuously write the data from the queue to the audio track:
-		while (!stopRequested) {
-			try {
-				// Get the next packet from the queue
-				packet = inputQueue.poll(1000, TimeUnit.MILLISECONDS);
-
-				if (packet == null) {
-					//Log.d(LOGTAG, "run: Queue is empty. skip this round");
-					continue;
-				}
-				//Log.v(LOGTAG, "packet rate: " + packet.getSampleRate()+'('+this.sampleRate+'*'+(packet.getSampleRate()/this.sampleRate)+')');
-				// apply audio filter (decimation)
-				if (packet.getSampleRate() > this.sampleRate) {
-					applyAudioFilter(packet, tempPacket);
-					filteredPacket = tempPacket;
-				} else
-					filteredPacket = packet;
-
-				// Convert doubles to shorts [expect doubles to be in [-1...1]
-				floatPacket = filteredPacket.re();
-				final int filteredPacketSize = filteredPacket.size();
-				//Log.v(LOGTAG, "filteredPacket size: " + filteredPacketSize);
-				//Log.v(LOGTAG, "filteredPacket: " + Arrays.toString(floatPacket));
-				for (int i = 0; i < filteredPacketSize; i++) {
-					shortPacket[i] = (short) (floatPacket[i] * 32767);
-				}
-				// Write it to the audioTrack:
-				if (audioTrack.write(shortPacket, 0, filteredPacketSize) != filteredPacketSize) {
-					Log.e(LOGTAG, "run: write() returned with error! stop");
-					stopRequested = true;
-				}
-
-				// Return the buffer to the output queue
-				outputQueue.offer(packet);
-			} catch (InterruptedException e) {
-				Log.e(LOGTAG, "run: Interrupted while polling from queue. stop");
+			// Convert doubles to shorts [expect doubles to be in [-1...1]
+			floatPacket = filteredPacket.re();
+			final int filteredPacketSize = filteredPacket.size();
+			//Log.v(LOGTAG, "filteredPacket size: " + filteredPacketSize);
+			//Log.v(LOGTAG, "filteredPacket: " + Arrays.toString(floatPacket));
+			for (int i = 0; i < filteredPacketSize; i++) {
+				shortPacket[i] = (short) (floatPacket[i] * 32767);
+			}
+			// Write it to the audioTrack:
+			if (audioTrack.write(shortPacket, 0, filteredPacketSize) != filteredPacketSize) {
+				Log.e(LOGTAG, "run: write() returned with error! stop");
 				stopRequested = true;
 			}
+
+			// Return the buffer to the output queue
+			outputQueue.offer(packet);
+		}
+		catch (InterruptedException e) {
+			Log.e(LOGTAG, "run: Interrupted while polling from queue. stop");
+			stopRequested = true;
+		}
+	}
+
+	// stop audio playback:
+	audioTrack.stop();
+	this.stopRequested = true;
+	Log.i(LOGTAG, "AudioSink stopped. (Thread: " + thread.getName() + ")");
+}
+
+/**
+ * Will apply the real array contained in input and decimate them to the audio rate.
+ *
+ * @param input  incoming (unfiltered) samples at the incoming rate (quadrature rate)
+ * @param output outgoing (filtered, decimated) samples at audio rate
+ */
+public void applyAudioFilter(SamplePacket input, SamplePacket output) {
+	// if we need a decimation of 8: apply first and second filter (decimate to input_rate/8)
+	final int inputSize = input.size();
+	final int decimation = input.getSampleRate() / sampleRate;
+	if (decimation == 8) {
+		// apply first filter (decimate to input_rate/2)
+		tmpAudioSamples.setSize(0);    // mark buffer as empty
+		if (audioFilter1.filterReal(input, tmpAudioSamples, 0, inputSize) < inputSize) {
+			Log.e(LOGTAG, "applyAudioFilter: [audioFilter1] could not filter all samples from input packet.");
 		}
 
-		// stop audio playback:
-		audioTrack.stop();
-		this.stopRequested = true;
-		Log.i(LOGTAG, "AudioSink stopped. (Thread: " + this.getName() + ")");
-	}
-
-	/**
-	 * Will apply the real array contained in input and decimate them to the audio rate.
-	 *
-	 * @param input  incoming (unfiltered) samples at the incoming rate (quadrature rate)
-	 * @param output outgoing (filtered, decimated) samples at audio rate
-	 */
-	public void applyAudioFilter(SamplePacket input, SamplePacket output) {
-		// if we need a decimation of 8: apply first and second filter (decimate to input_rate/8)
-		final int inputSize = input.size();
-		final int decimation = input.getSampleRate() / sampleRate;
-		if ( decimation == 8) {
-			// apply first filter (decimate to input_rate/2)
-			tmpAudioSamples.setSize(0);    // mark buffer as empty
-			if (audioFilter1.filterReal(input, tmpAudioSamples, 0, inputSize) < inputSize) {
-				Log.e(LOGTAG, "applyAudioFilter: [audioFilter1] could not filter all samples from input packet.");
-			}
-
-			// apply second filter (decimate to input_rate/8)
-			output.setSize(0);
-			final int tmpAudioSamplesSize = tmpAudioSamples.size();
-			if (audioFilter2.filterReal(tmpAudioSamples, output, 0, tmpAudioSamplesSize) < tmpAudioSamplesSize) {
-				Log.e(LOGTAG, "applyAudioFilter: [audioFilter2] could not apply all samples from input packet.");
-			}
-		} else if (decimation == 2) {
-			// apply first filter (decimate to input_rate/2 )
-			output.setSize(0);
-			if (audioFilter1.filterReal(input, output, 0, inputSize) < inputSize) {
-				Log.e(LOGTAG, "applyAudioFilter: [audioFilter1] could not apply all samples from input packet.");
-			}
-		} else
-			Log.e(LOGTAG, "applyAudioFilter: incoming sample rate is not supported!");
-	}
+		// apply second filter (decimate to input_rate/8)
+		output.setSize(0);
+		final int tmpAudioSamplesSize = tmpAudioSamples.size();
+		if (audioFilter2.filterReal(tmpAudioSamples, output, 0, tmpAudioSamplesSize) < tmpAudioSamplesSize) {
+			Log.e(LOGTAG, "applyAudioFilter: [audioFilter2] could not apply all samples from input packet.");
+		}
+	} else if (decimation == 2) {
+		// apply first filter (decimate to input_rate/2 )
+		output.setSize(0);
+		if (audioFilter1.filterReal(input, output, 0, inputSize) < inputSize) {
+			Log.e(LOGTAG, "applyAudioFilter: [audioFilter1] could not apply all samples from input packet.");
+		}
+	} else
+		Log.e(LOGTAG, "applyAudioFilter: incoming sample rate is not supported!");
+}
 }
